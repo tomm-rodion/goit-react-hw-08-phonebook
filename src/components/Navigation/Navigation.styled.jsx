6c4ea7b504b9bea6463239c074ac93a6328172dc
tomm-rodion <autovolna@gmail.com>
2023-09-30 23:51:11 +0300

    add BrowserRouter

---
+++ src/components/Navigation/Navigation.styled.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
+export const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+      </nav>
+    </>
+  );
+};
