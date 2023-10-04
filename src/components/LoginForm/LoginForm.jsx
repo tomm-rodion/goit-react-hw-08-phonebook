880e06f0444bb3ea87835de2f8a95421d22b246d
tomm-rodion <autovolna@gmail.com>
2023-10-04 12:16:39 +0300

    corection component LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,31 @@
 import { useDispatch } from 'react-redux';
-import { useFormik } from 'formik';
 import { logIn } from 'redux/auth/operations';
-import { LoginButton, LoginField } from './LoginForm.styled';
+import Notiflix from 'notiflix';
+import {
+  AuthButton,
+  AuthField,
+  AuthForm,
+} from 'components/RegisterForm/RegisterForm.styled';
+import { validationLogInSchema } from 'utils/validationSchema';
+import { useFormik } from 'formik';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
   function handleSubmit(values, { resetForm }) {
-    console.log(values);
     dispatch(
       logIn({
         email: values.email,
         password: values.password,
       })
-    );
+    )
+      .unwrap()
+      .then(() => Notiflix.Notify.success('You have successfully logged in'))
+      .catch(() =>
+        Notiflix.Notify.failure(
+          'Oops Something went wrong. Reload the page or check the fields: email, password!'
+        )
+      );
     resetForm();
   }
 
@@ -22,40 +34,41 @@ export const LoginForm = () => {
       email: '',
       password: '',
     },
+    validationSchema: validationLogInSchema,
     onSubmit: handleSubmit,
   });
 
   return (
-    <LoginForm onSubmit={formik.handleSubmit}>
-      <LoginField
+    <AuthForm onSubmit={formik.handleSubmit}>
+      <AuthField
         required
         autoComplete="off"
         fullWidth
         id="email"
         name="email"
         label="Email"
-        placeholder="batman@gmail.com"
+        placeholder="jacob12345@mail.com"
         value={formik.values.email}
         onChange={formik.handleChange}
         error={formik.touched.email && Boolean(formik.errors.email)}
         helperText={formik.touched.email && formik.errors.email}
       />
-      <LoginField
+      <AuthField
         required
         autoComplete="off"
         fullWidth
         id="password"
         name="password"
         label="Password"
-        placeholder="qwe12345"
+        placeholder="Jacob123"
         value={formik.values.password}
         onChange={formik.handleChange}
         error={formik.touched.password && Boolean(formik.errors.password)}
         helperText={formik.touched.password && formik.errors.password}
       />
-      <LoginButton fullWidth type="submit">
-        Log In
-      </LoginButton>
-    </LoginForm>
+      <AuthButton fullWidth type="submit">
+        Login
+      </AuthButton>
+    </AuthForm>
   );
 };
--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { Button, TextField } from '@mui/material';
-import { ContainerForm } from 'components/Style/mixins';
+import { ContainerForm, btn } from 'components/Style/mixins';
 
 export const LoginForm = styled.form`
   ${ContainerForm()}
@@ -11,8 +11,15 @@ export const LoginField = styled(TextField)`
   border-radius: 5px;
 `;
 export const LoginButton = styled(Button)`
-  color: #2dcf2d;
-  border: 1px solid #2dcf2d;
+  ${btn}
+  background-color:#f2f5f7;
+  color: #10567b;
+  border: 1px solid #10567b;
   padding: 10px 0;
   font-size: 20px;
+  &:hover {
+    background-color: #dadddc;
+    color: #10567b;
+    scale: 1.1;
+  }
 `;
